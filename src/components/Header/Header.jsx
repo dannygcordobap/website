7a531ed3fc9cde3d4380295fa523afc932070cc6
Danny Cordoba <dannygcordobap@gmail.com>
2023-01-26 13:34:26 -0800

    Modify resume component to redirect to pdf then reload

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import { isMobile } from 'react-device-detect';
 import '../../resources/styles/Header.css';
 
 const Header = ({ isLightUI, setIsLightUI }) => {
-
-    const resumePath = '../../resume/DanielCordobaPaez_Resume.pdf';
 
     const uiChangeHandler = () => {
         setIsLightUI(!isLightUI);
@@ -13,7 +10,7 @@ const Header = ({ isLightUI, setIsLightUI }) => {
     const linkToResume = (
         <a 
             className='toolbar-item' 
-            href={ isMobile ? resumePath : '/resume' } 
+            href='/resume' 
             rel="noreferrer" 
             target='_blank'
         >
--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,16 +1,15 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 export default function Resume() {
 
-    const resumePath = '../../resume/DanielCordobaPaez_Resume.pdf'
+    const resumePath = '../resume/DanielCordobaPaez_Resume.pdf'
+    const navigate = useNavigate();
 
-    return (
-        <div className='resume-container'>
-            <embed 
-                src={resumePath} 
-                title={'DanielCordobaPaez_Resume.pdf'} 
-                type='application/pdf' 
-                width='100%' 
-                height='100%'
-            />
-        </div>
-    )
+    useEffect(() => {
+        navigate(resumePath);
+        navigate(0);
+    });
+
+    return;
 }
