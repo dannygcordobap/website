08f1dc112162e5f70971c1015ce674491e67c164
Danny Cordoba <dannygcordobap@gmail.com>
2023-07-17 13:54:43 -0700

    Update resume

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -1,101 +1,100 @@
-import { useEffect, useState } from 'react';
-import { useSwipeable } from 'react-swipeable';
-import '../resources/styles/ImageCarousel.css';
+import { useEffect, useState } from "react";
+import { useSwipeable } from "react-swipeable";
+import "../resources/styles/ImageCarousel.css";
 
 export default function ImageCarousel({ images = [] }) {
-    const [current, setCurrent] = useState(0);
-    const [transform, setTransform] = useState({transform: 'translateX(0%)'})
-    const [prevClass, setPrevClass] = useState('disabled');
-    const [nextClass, setNextClass] = useState('');
+  const [current, setCurrent] = useState(0);
+  const [transform, setTransform] = useState({ transform: "translateX(0%)" });
+  const [prevClass, setPrevClass] = useState("disabled");
+  const [nextClass, setNextClass] = useState("");
 
-    const numberClickHandler = (e) => {
-        setCurrent(parseInt(e.target.id));
-        console.log(e.target.id);
-        console.log(e.target.className);
+  const numberClickHandler = (e) => {
+    setCurrent(parseInt(e.target.id));
+    console.log(e.target.id);
+    console.log(e.target.className);
+  };
+
+  const nextHandler = () => {
+    if (current < images.length - 1) {
+      setCurrent(current + 1);
     }
+  };
 
-    const nextHandler = () => {
-        if (current < images.length - 1) {
-            setCurrent(current + 1);
-        }
+  const prevHandler = () => {
+    if (current > 0) {
+      setCurrent(current - 1);
     }
+  };
 
-    const prevHandler = () => {
-        if (current > 0) {
-            setCurrent(current - 1);
-        }
+  const swipeHandlers = useSwipeable({
+    onSwipedLeft: nextHandler,
+    onSwipedRight: prevHandler,
+  });
+
+  const imageCount = [...Array(images.length).keys()].map((element) => {
+    return (
+      <div
+        className={"page-number" + (current === element ? " active" : "")}
+        id={element}
+        onClick={numberClickHandler}
+      >
+        {element + 1}
+      </div>
+    );
+  });
+
+  useEffect(() => {
+    if (current === 0) {
+      setPrevClass("disabled");
+      setNextClass("");
+    } else if (current + 1 === images.length) {
+      setNextClass("disabled");
+      setPrevClass("");
+    } else {
+      setPrevClass("");
+      setNextClass("");
     }
+    setTransform({
+      transform: `translateX(-${current * 100}%)`,
+    });
+  }, [current, images.length]);
 
-    const swipeHandlers = useSwipeable({
-        onSwipedLeft: nextHandler,
-        onSwipedRight: prevHandler
-    })
-
-    const imageCount = [...Array(images.length).keys()]
-        .map(element => {
-            return (
-                <div 
-                    className={'page-number' + (current === element ? ' active' : '')}
-                    id={ element }
-                    onClick={ numberClickHandler }
-                >
-                    { element + 1 }
-                </div>
-
-            )
-        });
-
-    useEffect(() => {
-        if (current === 0) {
-            setPrevClass('disabled');
-            setNextClass('');
-        } else if (current + 1 === images.length) {
-            setNextClass('disabled');
-            setPrevClass('');
-        } else {
-            setPrevClass('');
-            setNextClass('');
-        }
-        setTransform({
-            transform: `translateX(-${current * 100}%)`
-        })
-    }, [current, images.length])
-
-    return images.length > 0 ? (
-        <div className='carousel' {...swipeHandlers}>
-            <div className='inner' style={transform}>
-                {images.map(image => {
-                    return (
-                        <div className="carousel-item">
-                            <figure>
-                                <img src={image.src} alt={image.alt} />
-                                <figcaption>{image.caption}</figcaption>
-                            </figure>
-                        </div>
-                    )
-                })}
+  return images.length > 0 ? (
+    <div className="carousel" {...swipeHandlers}>
+      <div className="inner" style={transform}>
+        {images.map((image) => {
+          return (
+            <div className="carousel-item">
+              <figure>
+                <img src={image.src} alt={image.alt} />
+                <figcaption>{image.caption}</figcaption>
+              </figure>
             </div>
-            {images.length > 1 ? (
-                    <div className='image-count'>
-                        <div className={prevClass} onClick={prevHandler}>
-                            {'<<< Previous'}
-                        </div>
-                            {imageCount}
-                        <div className={nextClass} onClick={nextHandler}>
-                            {'Next >>>'}
-                        </div>
-                    </div>
-                ) : null
-            }
+          );
+        })}
+      </div>
+      {images.length > 1 ? (
+        <div className="image-count">
+          <div className={prevClass} onClick={prevHandler}>
+            {"<<< Previous"}
+          </div>
+          {imageCount}
+          <div className={nextClass} onClick={nextHandler}>
+            {"Next >>>"}
+          </div>
         </div>
-    ) : (
-        <div className='image-container'>
-            <div style={{
-                'margin': 'auto',
-                'align': 'center'
-            }}>
-                Image Carousel
-            </div>
-        </div>
-    )
+      ) : null}
+    </div>
+  ) : (
+    <div className="image-container">
+      <div
+        style={{
+          margin: "auto",
+          align: "center",
+        }}
+      >
+        Image Carousel
+      </div>
+    </div>
+  );
 }
--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,17 +1,18 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef } from "react";
 
 export default function Resume() {
+  const resumePath = "../../resume/DanielCordobaPaez-Resume.pdf";
+  const ref = useRef(null);
 
-    const resumePath = '../../resume/DanielCordobaPaez_Resume.pdf';
-    const ref = useRef(null);
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.click();
+    }
+  }, [ref]);
 
-    useEffect(() => {
-        if (ref.current) {
-            ref.current.click();
-        }
-    }, [ref]);
-
-    return (
-        <a hidden={true} href={resumePath} ref={ref}>Redirect</a>
-    );
+  return (
+    <a hidden={true} href={resumePath} ref={ref}>
+      Redirect
+    </a>
+  );
 }
