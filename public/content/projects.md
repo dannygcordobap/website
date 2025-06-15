821910123128d1eb9f775757bc3c01cdfb1fb025
Danny Cordoba <dannygcordobap@gmail.com>
2025-06-15 12:23:39 -0700

    Update content

---
+++ public/content/projects.md
@@ -0,0 +1,4 @@
+### Effortless Meal Plan
+Meal planning web app that won first place in the MagicHacks online hackathon from March 17, 2023, to March 19, 2023
+
+[Try it out](https://dannygcordobap.github.io/effortlessmealplan/)
---
+++ src/components/Home.jsx
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from "react";
+import ReactMarkdown from "react-markdown";
+
+export default function HomePage() {
+  const [aboutContent, setAboutContent] = useState("");
+  const [projectsContent, setProjectsContent] = useState("");
+  const [resumeContent, setResumeContent] = useState("");
+
+  useEffect(() => {
+    fetch("/content/about.md")
+      .then((res) => res.text())
+      .then((text) => setAboutContent(text));
+    fetch("/content/projects.md")
+      .then((res) => res.text())
+      .then((text) => setProjectsContent(text));
+    fetch("/content/resume.md")
+      .then((res) => res.text())
+      .then((text) => setResumeContent(text));
+  }, []);
+
+  return (
+    <div className="flex flex-col min-h-screen font-sans px-6 py-8">
+      <main className="flex-grow max-w-4xl mx-auto w-full">
+        {/* Header */}
+        <header className="mb-12 w-full">
+          <div className="flex items-center gap-4">
+            <div className="w-full">
+              <h1 className="text-4xl">Danny Cordoba</h1>
+              <p>
+                Hi, this is my website!
+              </p>
+            </div>
+          </div>
+        </header>
+
+        {/* About Me */}
+        <section>
+          <h1>
+            About Me
+          </h1>
+          <div className="max-w-none">
+            <ReactMarkdown>{aboutContent}</ReactMarkdown>
+          </div>
+        </section>
+
+        <hr />
+
+        {/* Experience */}
+        <section>
+          <h1>
+            Experience
+          </h1>
+          <div className="max-w-none">
+            <ReactMarkdown>{resumeContent}</ReactMarkdown>
+          </div>
+        </section>
+
+        <hr />
+
+        {/* Projects */}
+        <section>
+          <h1>
+            Projects
+          </h1>
+          <div className="max-w-none">
+            <ReactMarkdown>{projectsContent}</ReactMarkdown>
+          </div>
+        </section>
+      </main>
+
+      {/* Footer */}
+      <footer className="pt-6 max-w-4xl mx-auto w-full mt-10">
+        <hr />
+        <p className="mb-1">
+                  Contact: <a href="mailto:dannnygcordobap@gmail.com">dannnygcordobap@gmail.com</a>
+        </p>
+        <p className="mb-1">
+          GitHub: <a href="https://github.com/thursday">github.com/dannygcordobap</a>
+        </p>
+        <p className="mb-1">© {new Date().getFullYear()} Danny Cordoba</p>
+        <p>All rights reserved</p>
+      </footer>
+    </div>
+  );
+}
